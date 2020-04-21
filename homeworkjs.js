39c4859d609f23d36f0dc574f2c731acee3b5dfc
Stas0105 <sokolski@ukr.net>
2020-04-22 00:25:06 +0300

    First commit

---
+++ homeworkjs.js
@@ -0,0 +1,159 @@
+//№3
+/*let summa = +prompt('Введіть суму замовлення');
+if (summa < 500){
+    alert(summa / 100 * 99);
+}
+else if (summa > 500, summa < 1000){
+    alert(summa/100*95);
+}
+
+else if (summa > 1000){
+    alert(summa / 100 * 90 + (' та подарунковий сертифікат у 200грн'));
+   
+}
+//№4
+*/
+/*let firstquestion;
+let secondquestion;
+let thirdquestion;
+let fourthquestion;
+let fifthquestion;
+let summa;
+firstquestion = +prompt('скільки буде 2+2');
+secondquestion = +prompt('скільки буде 2+4');
+thirdquestion = +prompt('скільки буде 2+6');
+fourthquestion = +prompt('скільки буде 2+7');
+fifthquestion = +prompt('скільки буде 2+8');
+if (firstquestion == 4){
+    summa = 1;
+}
+else {
+    summa = 0;
+}
+if (secondquestion == 6){
+    summa = summa + 1;
+}
+else{
+    summa = summa + 0;
+}
+if (thirdquestion == 8){
+    summa = summa + 1;
+}
+else{
+    summa = summa + 0;
+}
+if (fourthquestion == 9){
+    summa = summa + 1;
+}
+else{
+    summa = summa + 0;
+}
+if (fifthquestion == 10){
+    summa = summa + 1;
+}
+else{
+    summa = summa + 0;
+}
+if (summa == 5 ){
+    alert('6');
+}
+else {
+    alert(summa);
+}
+*/
+//№6
+/*let number = prompt('натисніть копку від 1 до =');
+if( number == 1){
+    alert('!')
+}
+else if (number == 2){
+    alert ('@')
+}
+else if (number == 3){
+    alert ('#')
+}
+else if (number == 4){
+    alert ('$')
+}
+else if (number == 5){
+    alert ('%')
+}
+else if (number == 6){
+    alert ('^')
+}
+else if (number == 7){
+    alert ('&')
+}
+else if (number == 8){
+    alert ('*')
+}
+else if (number == 9){
+    alert ('(')
+}
+else if (number == 0){
+    alert (')')
+}
+else if (number == '-'){
+    alert ('_')
+}
+else if (number == '='){
+    alert ('+')
+}
+else{
+    alert('некоректно введено номер')
+}
+*/
+//№5
+/*
+let sovpadenie = 0;
+var str = prompt('введыть люе значення')
+var res = str.split("");
+if (res[0] === res[1] && res[1]===res[2]){
+    sovpadenie = sovpadenie + 3;
+alert(`${sovpadenie}  одинакові цифри`);
+}
+
+else if (res[0] === res[1] && res[0] !== res[2] ){
+    sovpadenie = sovpadenie + 2;
+alert(`${sovpadenie}  одинакові цифри`);
+}
+
+if (res[1] === res[2] && res[1] !== res[0] ){
+    sovpadenie = sovpadenie + 2;
+    alert(`${sovpadenie}  одинакові цифри`);
+}
+if (res[0] === res[2] && res[1] !== res[2] ){
+    sovpadenie = sovpadenie + 2;
+    alert(`${sovpadenie}  одинакові цифри`);
+}
+else{
+    alert('немає одинакових чисел')
+}
+*/
+//№2
+   /*let uan = +prompt('Введи кількість гривень');
+    if (uan === null || uan === undefined || uan === false){
+        alert('Введіть коректне значення');
+    }
+   else{
+    let dollar = 28;
+    let euro = 29;
+    let brent = 742;
+    let gold = 1591.29;
+    alert(
+        `${uan/dollar} USD
+         ${uan/euro} EUR
+         ${uan/brent} BRENT
+         ${uan/gold} GOLD`
+    )
+}
+*/
+   
+  
+
+
+ 
+
+
+
+
